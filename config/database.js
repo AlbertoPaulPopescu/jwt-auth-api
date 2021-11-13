d298d07298f8c789799775f580fe1e8e1fbf8aa2
unknown <albertopopescu@outlook.com>
2021-11-13 02:38:04 +0000

    use mysql

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mysql'),
 
   /*
   |--------------------------------------------------------------------------
@@ -48,11 +48,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
+      host: Env.get('DB_HOST', '127.0.0.1'),
+      port: Env.get('DB_PORT', '3306'),
       user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      password: Env.get('DB_PASSWORD', 'password'),
+      database: Env.get('DB_DATABASE', 'cwebbing')
     },
     debug: Env.get('DB_DEBUG', false)
   },
